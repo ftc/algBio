e1d87eb329fd6dae46eb498952b320e64e12800a
Shawn Meier <shawn.meier@gmail.com>
2013-11-26 12:16:01 -0700

    added other properties to amino.py to calculate scoring matrix

--- amino.py
+++ amino.py
@@ -1,14 +1,21 @@
 filename = "amino_acids"
 splitChar = " "
-#NOTE: do not access aminoMap directly, call getAminoMap
 aminoMap = {} #create empty map
+aminoPolarity = {}
+aminoHydrop = {}
 f = open(filename,'r')
 for i in f:
 	c = i.split(splitChar)
 	aminoMap[c[0]] = c[1].strip()
+	aminoPolarity[c[0]] = c[2].strip()
+	aminoHydrop[c[0]] = float(c[3].strip())
 
 def getAmino():
 	return aminoMap.copy()
+def getPolarity():
+	return aminoPolarity.copy()
+def getHydrop():
+	return aminoHydrop.copy()
 
 def getOrderedArray():
 	return sorted(aminoMap.keys())
